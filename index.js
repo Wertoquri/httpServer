106b0f79d9cc88bb79277c7c7cba747ef27e33af
[KyryloPoidiuk] <kirilpojduk@gmail.com>
2024-02-03 19:43:56 +0200

    update

--- index.js
+++ index.js
@@ -6,30 +6,43 @@ import { fileURLToPath } from "url"
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
-let server = http.createServer(function(request, response){
+let server = http.createServer(function (request, response) {
     let pub = path.join(__dirname, "public");
-    let filePath;
-    switch(request.url){
-        case "/":
-            response.writeHead(200, {"content-type": "text/html"})
-            filePath = path.join(pub, "index.html")
-            break
-        case "/contacts":
-            response.writeHead(200, {"content-type": "text/html"})
-            filePath = path.join(pub, "contacts.html")
-            break
-        case "/style.css":
-            response.writeHead(200, {"content-Type": "text/css"})
-            filePath = path.join(pub, "style.css")
-            break
-        default: 
-            response.writeHead(404)
-            response.end("<h1 style='font-size: 100px'>404</h1>")
+    if (request.url == "/") {
+        response.writeHead(200, { "content-type": "text/html" })
+        let content = fs.readFileSync(path.join(pub, "index.html"))
+        response.end(content)
 
+    } else if (request.url == "/style.css") {
+        response.writeHead(200, { "content-type": "text/css" })
+        let content = fs.readFileSync(path.join(pub, "style.css"))
+        response.end(content)
+
+    } else if (request.url == "/add" && request.method == "POST") {
+        let data = ""
+        request.on("data", function (chunk) {
+            data += chunk;
+        })
+        request.on("end", function () {
+            let usp = new URLSearchParams(data)
+            console.log(usp.get("author"))
+            console.log(usp.get("content"))
+            let content = `
+            <div>
+            <h3>${usp.get("author")}</h3>
+            <p>${usp.get("content")}</p>
+            </div>
+            
+            `
+            fs.appendFileSync(path.join(pub, "index.html"), content)
+            response.writeHead(302, { "location": "/" })
+            response.end()
+        })
     }
-    let content = fs.readFileSync(filePath)
-    response.end(content)
-
+    else {
+        rewponse.writeHead(404)
+        response.end("<h1>404 not found</h1>")
+    }
 })
 
 server.listen(3000)
